2cc7f4f9d814631c8e8e7f733aec219c183adf43
pantchayan <pantchayan@gmail.com>
2020-08-26 11:27:28 +0530

    26th August

--- Graphs/questions.cpp
+++ Graphs/questions.cpp
@@ -309,7 +309,36 @@ int orangesRotting(vector<vector<int>> &grid)
         }
     
     }
-
+// Leetcode 547 UnionFind for GCC in Matrix -- Friend circles ==========================================================================
+    int findCircleNum(vector<vector<int>>& M) {
+        int n = M.size();
+        
+        vector<int> par;
+        for(int i=0;i<n;i++){
+            par.push_back(i);
+        }
+        
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(i!=j && M[i][j]==1){
+                    int l1 = findPar(i,par);
+                    int l2 = findPar(j,par);  
+                    if(l1==l2){
+                        continue;
+                    }
+                    else{
+                        count++;
+                        par[l1]= l2;
+                    }
+                }
+            }
+        }
+        
+        
+        return n-count;
+        
+    }
 
 
 //=======================================================================================================================================
