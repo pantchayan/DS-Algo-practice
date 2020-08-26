a2eb5143818fae0c7cc803b58cd84b8f1e4722d8
pantchayan <pantchayan@gmail.com>
2020-08-26 22:25:12 +0530

    26th August

--- Graphs/questions.cpp
+++ Graphs/questions.cpp
@@ -400,6 +400,45 @@ int numIslands(vector<vector<char>> &grid)
     return numOfOnes - mergeCount;
 }
 
+
+// leetcode 839 ======================================================================================================================
+
+
+    vector<int> par;
+    bool isSimilar(string &A, string &B){
+        
+        //if(A==B) return true;
+        int count = 0;
+        for(int i=0;i<A.size();i++){
+            if(A[i]!=B[i] && ++count>2)
+                return false;
+        }
+        return true;
+    }
+    
+    int numSimilarGroups(vector<string>& A) {
+        int n = A.size();
+        
+        for(int i=0;i<n;i++){
+            par.push_back(i);
+        }
+        
+        int groups = n;
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isSimilar(A[i],A[j])){
+                    int l1 = findPar(i,par);
+                    int l2 = findPar(j,par);
+                    if(l1!=l2){
+                        par[l1] = l2;
+                        groups--;
+                    }
+                }
+            }
+        }
+        return groups;
+    }
 //=======================================================================================================================================
 
 void setDFS()
