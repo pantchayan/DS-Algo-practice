701b04135de9cece6b1b7463a1a95a263c490d70
pantchayan <pantchayan@gmail.com>
2020-09-11 18:57:18 +0530

    11th Sept

--- Graphs/questions.cpp
+++ Graphs/questions.cpp
@@ -401,83 +401,94 @@ int numIslands(vector<vector<char>> &grid)
     return numOfOnes - mergeCount;
 }
 
-
 // leetcode 839 ======================================================================================================================
 
+vector<int> par;
+bool isSimilar(string &A, string &B)
+{
 
-    vector<int> par;
-    bool isSimilar(string &A, string &B){
-        
-        //if(A==B) return true;
-        int count = 0;
-        for(int i=0;i<A.size();i++){
-            if(A[i]!=B[i] && ++count>2)
-                return false;
-        }
-        return true;
+    //if(A==B) return true;
+    int count = 0;
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (A[i] != B[i] && ++count > 2)
+            return false;
     }
-    
-    int numSimilarGroups(vector<string>& A) {
-        int n = A.size();
-        
-        for(int i=0;i<n;i++){
-            par.push_back(i);
-        }
-        
-        int groups = n;
-        
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(isSimilar(A[i],A[j])){
-                    int l1 = findPar(i,par);
-                    int l2 = findPar(j,par);
-                    if(l1!=l2){
-                        par[l1] = l2;
-                        groups--;
-                    }
+    return true;
+}
+
+int numSimilarGroups(vector<string> &A)
+{
+    int n = A.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        par.push_back(i);
+    }
+
+    int groups = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (isSimilar(A[i], A[j]))
+            {
+                int l1 = findPar(i, par);
+                int l2 = findPar(j, par);
+                if (l1 != l2)
+                {
+                    par[l1] = l2;
+                    groups--;
                 }
             }
         }
-        return groups;
     }
+    return groups;
+}
 
 // Leetcode  - Water Distribution in a village ==========================================================================================
 
+int findPar(int vtx, vector<int> &par)
+{
+    if (par[vtx] == vtx)
+        return vtx;
 
-int findPar(int vtx,vector<int>& par){
-    if(par[vtx] == vtx) return vtx;
-
-    return par[vtx] = findPar(par[vtx],par);
+    return par[vtx] = findPar(par[vtx], par);
 }
 
-int waterDistribution(int n,vector<int>& wells,vector<vector<int>>& pipes){
-    //pipes -- > (u,v,w) 
+int waterDistribution(int n, vector<int> &wells, vector<vector<int>> &pipes)
+{
+    //pipes -- > (u,v,w)
 
-    for(int i=0;i<n;i++){
-        vector<int> newEdge = {0,i,wells[i]};
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> newEdge = {0, i, wells[i]};
         pipes.push_back(newEdge);
     }
 
     // sorting the 2d array pipes w.r.t. weights coloumn =========================================================
     std::sort(pipes.begin(),
-          pipes.end(),
-          [] (const std::vector<double> &a, const std::vector<double> &b)
-          {
-              return a[2] < b[2];
-          });
+              pipes.end(),
+              [](const std::vector<double> &a, const std::vector<double> &b) {
+                  return a[2] < b[2];
+              });
 
     vector<int> par;
 
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         par.push_back(i);
     }
 
     int cost = 0;
-    for(vector<int> edge:pipes){
-        int p1 = findPar(edge[0],par);
-        int p2 = findPar(edge[1],par);
-         
-        if(p1!=p2){
+    for (vector<int> edge : pipes)
+    {
+        int p1 = findPar(edge[0], par);
+        int p2 = findPar(edge[1], par);
+
+        if (p1 != p2)
+        {
             par[p2] = p1;
             cost = cost + edge[2];
         }
@@ -486,10 +497,8 @@ int waterDistribution(int n,vector<int>& wells,vector<vector<int>>& pipes){
     return cost;
 }
 
-
 // Mr. President HackerEarth ===========================================================================================================
 // https://www.hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/practice-problems/algorithm/mr-president/submissions/
-
 
 int mrPresident()
 {
@@ -515,8 +524,8 @@ int mrPresident()
     lli MSTCost = 0;
     for (vector<int> g : graph)
     {
-        int p1 = findPar(g[0],par);
-        int p2 = findPar(g[1],par);
+        int p1 = findPar(g[0], par);
+        int p2 = findPar(g[1], par);
         if (p1 != p2)
         {
             par[p1] = p2;
@@ -549,7 +558,6 @@ auto SpeedUp = []() {
     return 0;
 }();
 
-
 //=======================================================================================================================================
 
 void setDFS()
@@ -567,6 +575,92 @@ int main()
     return 0;
 }
 
-
 //question link: https://leetcode.com/problems/minimize-malware-spread/discuss/614031/C++-:-Union-Find-(pepcoding.com)-reframe-the-question-on-"CORONA"-with-relatable-explanation
 
+// Leetcode 743 - Network Delay Time (Dijkstra's)===========================================================================================================
+
+
+class npair
+{
+public:
+    int src = 0;
+    int par = 0;
+    int w = 0;
+    int wsf = 0;
+
+    npair(int src, int par, int w, int wsf)
+    {
+        this->src = src;
+        this->par = par;
+        this->w = w;
+        this->wsf = wsf;
+    }
+};
+
+struct nComp
+{
+public:
+    bool operator()(npair &p1, npair &p2)
+    {
+        return p1.wsf > p2.wsf; // default min PQ.
+        //   return p2.wsf > p1.wsf   // max PQ.
+    }
+};
+
+int networkDelayTime(vector<vector<int>> &times, int N, int K)
+{
+    // BFS
+
+    //int src = K;
+    priority_queue<npair, vector<npair>, nComp> que;
+
+    que.push(npair(K, -1, 0, 0));
+
+    vector<bool> vis(N + 1, false);
+
+    int maxWsf = 0;
+
+    while (que.size() != 0)
+    {
+        int size = que.size();
+        while (size-- > 0)
+        {
+            npair vtx = que.top();
+            que.pop();
+            //cycle check
+            if (vis[vtx.src])
+                continue;
+
+            if (vtx.par != -1)
+            {
+                maxWsf = max(maxWsf, vtx.wsf);
+            }
+
+            vis[vtx.src] = true;
+
+            for (int i = 0; i < times.size(); i++)
+            {
+                if (times[i][0] == vtx.src)
+                {
+                    int v = times[i][1];
+
+                    if (!vis[v])
+                    {
+                        int w = times[i][2];
+                        que.push(npair(v, vtx.src, w, vtx.wsf + w));
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i < vis.size(); i++)
+    {
+        if (vis[i] == false)
+        {
+            return -1;
+        }
+    }
+
+    return maxWsf;
+}
