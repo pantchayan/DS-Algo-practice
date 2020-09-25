d1e66433e2378080e0093ddbef8a259048efb7e6
pantchayan <pantchayan@gmail.com>
2020-09-25 23:51:04 +0530

    25th Sept

--- Trees/Tree.java
+++ Trees/Tree.java
@@ -289,6 +289,36 @@ public class Tree {
         return myRes;
     }
 
+
+
+    // Level Order Traversal ======================================================================================
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+        
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     if(root==null) return ans;
+    //     LinkedList<TreeNode> que = new LinkedList<TreeNode>();
+        
+    //     que.addLast(root);
+        
+    //     while(que.size()!=0){
+    //         int size = que.size();
+    //         List<Integer> smallAns = new ArrayList<>();
+    //         while(size-->0){
+    //             TreeNode curr = que.getFirst();
+    //             que.removeFirst();
+                
+    //             smallAns.add(curr.val);
+                
+    //             if(curr.left!=null) que.addLast(curr.left);
+                
+    //             if(curr.right!=null) que.addLast(curr.right);
+                
+    //         }
+    //         ans.add(smallAns);
+    //     }
+    //     return ans;
+    // }
+
     public static void set2() {
         // lets code for allNodesKaway
         // System.out.println();
