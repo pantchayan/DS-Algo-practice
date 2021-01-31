8d4410505be2cb24936edb1d01649bc84e5e8546
pantchayan <pantchayan@gmail.com>
2021-01-31 16:10:26 +0530

    31 1 21

--- Pep-PP/Foundation/String/Questions.java
+++ Pep-PP/Foundation/String/Questions.java
@@ -94,6 +94,31 @@ public class Questions {
         return ans;
     }
 
+    // ITERATIVE PERMUTATIONS =============================================================================
+    public static void permutations(String str) {
+        int n = factorial(str.length());
+        for (int i = 0; i < n; i++) {
+            StringBuilder s = new StringBuilder(str);
+            int dvd = i;
+            for (int div = str.length(); div >= 1; div--) {
+                int q = dvd / div;
+                int r = dvd % div;
+                System.out.print(s.charAt(r));
+                dvd = q;
+                s.deleteCharAt(r);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int factorial(int n) {
+        int ans = 1;
+        for (int i = 2; i <= n; i++) {
+            ans *= i;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String str = scn.next();
