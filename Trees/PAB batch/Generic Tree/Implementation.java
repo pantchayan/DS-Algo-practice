6da083472576f1aaa61c19887146a35c69357027
pantchayan <pantchayan@gmail.com>
2021-04-06 21:17:24 +0530

    Diameter of Generic Tree

--- Trees/PAB batch/Generic Tree/Implementation.java
+++ Trees/PAB batch/Generic Tree/Implementation.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Stack;
+
 public class Implementation {
     public static class GenericTree {
         Node root;
+
         public static class Node {
             int data;
             ArrayList<Node> children;
@@ -16,18 +18,18 @@ public class Implementation {
                 this.data = data;
             }
         }
+
         public Node construct(Integer[] input) {
             Node root = new Node(input[0]);
 
             Stack<Node> st = new Stack<>();
             st.push(root);
-            for(int i=1;i<input.length;i++){
+            for (int i = 1; i < input.length; i++) {
                 Integer val = input[i];
 
-                if(val==null){
+                if (val == null) {
                     st.pop();
-                }
-                else{
+                } else {
                     Node node = new Node(val);
                     st.peek().children.add(node);
                     st.push(node);
@@ -36,61 +38,109 @@ public class Implementation {
             return root;
         }
 
-        public void display(Node node){
-            System.out.print(node.data+" -> ");
-            for(Node child:node.children){
-                System.out.print(child.data+" ");
+        public void display(Node node) {
+            System.out.print(node.data + " -> ");
+            for (Node child : node.children) {
+                System.out.print(child.data + " ");
             }
             System.out.print(".\n");
-            for(Node child:node.children){
+            for (Node child : node.children) {
                 // System.out.print(child.data+" -> ");
                 display(child);
             }
         }
 
+        public int height(Node node) {
+            // h -> node's height
+            // ch -> child's height
+            int h = -1;
+
+            for (Node child : node.children) {
+                int ch = height(child);
+                if (ch > h) {
+                    h = ch;
+                }
+            }
+
+            return h + 1;
+        }
+
+        int dia;
+
+        public int diameter(Node node) {
+            int lh = -1, slh = -1;
+
+            for (Node child : node.children) {
+                int ch = diameter(child);
+
+                if (ch > lh) {
+                    slh = lh;
+                    lh = ch;
+                } else if (ch > slh) {
+                    slh = ch;
+                }
+            }
+
+            if (lh + slh + 2 > dia)
+                dia = lh + slh + 2;
+
+            return lh + 1;
+        }
+
         int ceil;
         int floor;
-        public void ceilAndFloor(Node node, int val){
 
-            if(node.data>val){
-                if(node.data < ceil){
+        public void ceilAndFloor(Node node, int val) {
+
+            if (node.data > val) {
+                if (node.data < ceil) {
                     ceil = node.data;
                 }
             }
 
-            if(node.data<val){
-                if(node.data > floor){
+            if (node.data < val) {
+                if (node.data > floor) {
                     floor = node.data;
                 }
             }
 
-            for(Node child : node.children){
+            for (Node child : node.children) {
                 ceilAndFloor(child, val);
             }
         }
 
-        public void KthLargest(Node node, int k){
+        public void KthLargest(Node node, int k) {
             int ans = Integer.MAX_VALUE;
-            
-            for(int i=0;i<k;i++){
+
+            for (int i = 0; i < k; i++) {
                 // ceil = Integer.MAX_VALUE;
                 floor = Integer.MIN_VALUE;
-            
+
                 ceilAndFloor(node, ans);
                 ans = floor;
             }
-            System.out.println(k+"th Largest Element is : "+ans);
+            System.out.println(k + "th Largest Element is : " + ans);
         }
+
     }
 
-    public static void Set2(GenericTree tree1){
+    public static void Set2(GenericTree tree1) {
         tree1.ceil = Integer.MAX_VALUE;
         tree1.floor = Integer.MIN_VALUE;
         tree1.ceilAndFloor(tree1.root, 120);
-        System.out.println("CEIL : "+tree1.ceil+"\nFLOOR : "+tree1.floor);
-
+        System.out.println("CEIL : " + tree1.ceil + "\nFLOOR : " + tree1.floor);
 
         tree1.KthLargest(tree1.root, 8);
+    }
+
+    public static void Set1(GenericTree tree) {
+        int height = tree.height(tree.root);
+        System.out.println(height);
+
+        tree.dia = 0;
+        height = tree.diameter(tree.root);
+        System.out.println(height+" "+tree.dia);
+
     }
 
     public static void main(String[] args) {
@@ -102,7 +152,8 @@ public class Implementation {
 
         tree1.display(tree1.root);
 
-        Set2(tree1);
+        Set1(tree1);
+        // Set2(tree1);
     }
 
 }
