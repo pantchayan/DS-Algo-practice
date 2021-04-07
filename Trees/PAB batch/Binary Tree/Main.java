c3c116a6eaab7f6813aa23a08ac7cd077b4bf7c5
pantchayan <pantchayan@gmail.com>
2021-04-07 21:53:20 +0530

    Binary Tree

--- Trees/PAB batch/Binary Tree/Main.java
+++ Trees/PAB batch/Binary Tree/Main.java
@@ -79,6 +79,26 @@ public class Main{
         
         }
 
+        public int size(Node node){
+            if(node==null) return 0;
+            return size(node.left) + size(node.right) + 1;
+        }
+
+        public int sum(Node node){
+            if(node==null) return 0;
+            return sum(node.left) + sum(node.right) + node.data;
+        }
+        
+        public int height(Node node){
+            if(node==null) return -1;
+            return Math.max(height(node.left) , height(node.right)) + 1;
+        }
+        
+        public int max(Node node){
+            if(node==null) return Integer.MIN_VALUE;
+            return Math.max(Math.max(max(node.left), max(node.right)),node.data);
+        }
+
         public void preOrder(Node node){
             if(node == null){
                 System.out.print("NULL"+" ");
@@ -89,9 +109,14 @@ public class Main{
             preOrder(node.left);
             preOrder(node.right);
         }
-
     }
 
+    public static void basic(BinaryTree tree){
+        System.out.println("Max : "+tree.max(tree.root));
+        System.out.println("Height : "+tree.height(tree.root));
+        System.out.println("Sum : "+tree.sum(tree.root));
+        System.out.println("Size : "+tree.size(tree.root));
+    }
 
     public static void main(String[] args) {
         BinaryTree tree1 = new BinaryTree();
@@ -101,5 +126,6 @@ public class Main{
         // tree1.preOrder(tree1.root);
         // System.out.println();
         tree1.display(tree1.root);
+        basic(tree1);
     }
 }
